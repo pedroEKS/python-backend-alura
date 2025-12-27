f4f7f49eab040ac7af10ed90db192383dbe293d5
Pedro Aureliano <pedroeks155@gmail.com>
2025-12-27 13:15:30 -0300

    Add initial implementation of restaurant management options

---
+++ New/aula01.py
@@ -0,0 +1,18 @@
+'''
+print("Hello world!")
+
+print("Olá mundo!") # para você que não manja dos inglish 
+'''
+print('Sabor Express!\n')
+print('1. Cadastro de Restaurante.')
+
+print('2. Listar de Restaurante.')
+
+print('3. Ativar de Restaurante.')
+
+print('4. Sair.\n')
+
+option_chose = input('Escolha uma opção: ')
+print(f'Você escolheu a opção {option_chose}.')
+
+
