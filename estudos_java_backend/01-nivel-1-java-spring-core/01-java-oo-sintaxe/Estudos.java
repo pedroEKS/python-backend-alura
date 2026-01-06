a43e2044ac957d25030ef75e237442fa9c776cb1
Pedro Aureliano <pedroeks155@gmail.com>
2026-01-06 16:47:35 -0300

    Desafios em prática

---
+++ estudos_java_backend/01-nivel-1-java-spring-core/01-java-oo-sintaxe/Estudos.java
@@ -0,0 +1,7 @@
+public class Estudos {
+    public static void main(String[] args) {
+        System.out.println("--- Anotações de Estudos ---");
+        System.out.println("JVM (Máquina Virtual Java) - Executa o código.");
+        System.out.println("JRE (Ambiente de Execução) - Onde o Java roda.");
+    }
+}
---
+++ estudos_java_backend/01-nivel-1-java-spring-core/01-java-oo-sintaxe/Perfil.java
@@ -0,0 +1,9 @@
+public class Perfil {
+    public static void main(String[] args) {
+        // Mensagem 1
+        System.out.println("Olá, Dateks!");
+
+        // Mensagem 2
+        System.out.println("Tudo bem?");
+    }
+}
